c270f7821b6bd05cb9d564693d8ed62992c6dad2
Anastasiia-Zatvornytska <nastulja@gmail.com>
2026-09-03 02:28:41 +0300

    task-2 done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,39 @@
-console.log('ghrjlkgkkerg');
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const delay = Number(document.querySelector("[name='delay']").value);
+    const state = document.querySelector("input[name='state']:checked").value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === "fulfilled") {
+                resolve(delay);
+            }
+            else {
+                reject(delay);
+
+            }
+
+        }, delay);
+    });
+
+    promise
+        .then((delay) => {
+        iziToast.success({
+    title: 'OK',
+    message: `✅ Fulfilled promise in ${delay}ms`
+});
+        })
+        .catch((delay) => {
+        iziToast.error({
+    title: 'Error',
+    message: `❌ Rejected promise in ${delay}ms`
+});
+    })
+});
